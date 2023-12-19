tjajt/podman-desktop

commit 036d78584a984ee84231e6347e611f78b96d2020

feat: add tabs to containers list page (#4600)
* feat: add tabs to containers list page
Signed-off-by: Philippe Martin <phmartin@redhat.com>

* refactor: move searchTerm out of state
Signed-off-by: Philippe Martin <phmartin@redhat.com>

* feat: adapt empty list mesage depending on tab
Signed-off-by: Philippe Martin <phmartin@redhat.com>

* feat: adapt filter message
Signed-off-by: Philippe Martin <phmartin@redhat.com>

* fix: display all containers and filtered count
Signed-off-by: Philippe Martin <phmartin@redhat.com>

* test: unit tests
Signed-off-by: Philippe Martin <phmartin@redhat.com>

* refactor: add a tab type to the Button component
Signed-off-by: Philippe Martin <phmartin@redhat.com>

packages/renderer/src/lib/container/ContainerList.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { onDestroy, onMount } from 'svelte';
-import { filtered, searchPattern, containersInfos } from '../../stores/containers';
+import { containersInfos } from '../../stores/containers';
 import { viewsContributions } from '../../stores/views';
 import { context } from '../../stores/context';
 import type { ContainerInfo } from '../../../../main/src/plugin/api/container-info';
@@ -40,6 +40,7 @@ import Button from '../ui/Button.svelte';
 import StateChange from '../ui/StateChange.svelte';
 import SolidPodIcon from '../images/SolidPodIcon.svelte';
 import ProviderInfo from '../ui/ProviderInfo.svelte';
+import { findMatchInLeaves } from '../../stores/search-util';
 
 const containerUtils = new ContainerUtils();
 let openChoiceModal = false;
@@ -51,7 +52,7 @@ let viewContributions: ViewInfoUI[] = [];
 let globalContext: ContextUI;
 let containersInfo: ContainerInfo[] = [];
 export let searchTerm = '';
-$: searchPattern.set(searchTerm);
+$: updateContainers(containersInfo, globalContext, viewContributions, searchTerm);
 
 function fromExistingImage(): void {
   openChoiceModal = false;
@@ -230,19 +231,19 @@ onMount(async () => {
   contextsUnsubscribe = context.subscribe(value => {
     globalContext = value;
     if (containersInfo.length > 0) {
-      updateContainers(containersInfo, globalContext, viewContributions);
+      updateContainers(containersInfo, globalContext, viewContributions, searchTerm);
     }
   });
 
   viewsUnsubscribe = viewsContributions.subscribe(value => {
     viewContributions = value.filter(view => view.viewId === CONTAINER_LIST_VIEW) || [];
     if (containersInfo.length > 0) {
-      updateContainers(containersInfo, globalContext, viewContributions);
+      updateContainers(containersInfo, globalContext, viewContributions, searchTerm);
     }
   });
 
-  containersUnsubscribe = filtered.subscribe(value => {
-    updateContainers(value, globalContext, viewContributions);
+  containersUnsubscribe = containersInfos.subscribe(value => {
+    updateContainers(value, globalContext, viewContributions, searchTerm);
   });
 
   podUnsubscribe = podsInfos.subscribe(podInfos => {
@@ -250,7 +251,17 @@ onMount(async () => {
   });
 });
 
-function updateContainers(containers: ContainerInfo[], globalContext: ContextUI, viewContributions: ViewInfoUI[]) {
+/* updateContainers updates the variables:
+   - containersInfo with the value of containers
+   - containerGroups based on the containers and their groups
+   - multipleEngines and enginesList based on the engines of containers
+*/
+function updateContainers(
+  containers: ContainerInfo[],
+  globalContext: ContextUI,
+  viewContributions: ViewInfoUI[],
+  searchTerm: string,
+) {
   containersInfo = containers;
   const currentContainers = containers.map((containerInfo: ContainerInfo) => {
     return containerUtils.getContainerInfoUI(containerInfo, globalContext, viewContributions);
@@ -271,7 +282,26 @@ function updateContainers(containers: ContainerInfo[], globalContext: ContextUI,
   enginesList = uniqueEngines;
 
   // create groups
-  const computedContainerGroups = containerUtils.getContainerGroups(currentContainers);
+  let computedContainerGroups = containerUtils.getContainerGroups(currentContainers);
+
+  // Filter containers in groups
+  computedContainerGroups.forEach(group => {
+    group.containers = group.containers
+      .filter(containerInfo =>
+        findMatchInLeaves(containerInfo, containerUtils.filterSearchTerm(searchTerm).toLowerCase()),
+      )
+      .filter(containerInfo => {
+        if (containerUtils.filterIsRunning(searchTerm)) {
+          return containerInfo.state === 'RUNNING';
+        }
+        if (containerUtils.filterIsStopped(searchTerm)) {
+          return containerInfo.state !== 'RUNNING';
+        }
+        return true;
+      });
+  });
+  // Remove groups with all containers filtered
+  computedContainerGroups = computedContainerGroups.filter(group => group.containers.length > 0);
 
   // update selected items based on current selected items
   computedContainerGroups.forEach(group => {
@@ -297,6 +327,14 @@ function updateContainers(containers: ContainerInfo[], globalContext: ContextUI,
   // compute refresh interval
   const interval = computeInterval();
   refreshTimeouts.push(setTimeout(refreshUptime, interval));
+}
+
+function displayContainersCount(containerGroup: ContainerGroupInfoUI) {
+  let result = containerGroup.allContainersCount + ' container' + (containerGroup.allContainersCount > 1 ? 's' : '');
+  if (containerGroup.containers.length !== containerGroup.allContainersCount) {
+    result += ` (${containerGroup.allContainersCount - containerGroup.containers.length} filtered)`;
+  }
+  return result;
 }
 
 onDestroy(() => {
@@ -402,6 +440,18 @@ function errorCallback(container: ContainerInfoUI, errorMessage: string): void {
   container.state = 'ERROR';
   containerGroups = [...containerGroups];
 }
+
+function resetRunningFilter() {
+  searchTerm = containerUtils.filterResetRunning(searchTerm);
+}
+
+function setRunningFilter() {
+  searchTerm = containerUtils.filterSetRunning(searchTerm);
+}
+
+function setStoppedFilter() {
+  searchTerm = containerUtils.filterSetStopped(searchTerm);
+}
 </script>
 
 <NavPage bind:searchTerm="{searchTerm}" title="containers">
@@ -430,6 +480,15 @@ function errorCallback(container: ContainerInfoUI, errorMessage: string): void {
         icon="{SolidPodIcon}" />
       <span class="pl-2">On {selectedItemsNumber} selected items.</span>
     {/if}
+  </div>
+
+  <div class="flex flex-row px-2 mb-2 border-b border-charcoal-400" slot="tabs">
+    <Button type="tab" on:click="{() => resetRunningFilter()}" selected="{containerUtils.filterIsAll(searchTerm)}"
+      >All containers</Button>
+    <Button type="tab" on:click="{() => setRunningFilter()}" selected="{containerUtils.filterIsRunning(searchTerm)}"
+      >Running containers</Button>
+    <Button type="tab" on:click="{() => setStoppedFilter()}" selected="{containerUtils.filterIsStopped(searchTerm)}"
+      >Stopped containers</Button>
   </div>
 
   <div class="flex min-w-full h-full" slot="content">
@@ -489,7 +548,7 @@ function errorCallback(container: ContainerInfoUI, errorMessage: string): void {
                       {containerGroup.name} ({containerGroup.type})
                     </button>
                     <div class="text-xs font-extra-light text-gray-900">
-                      {containerGroup.containers.length} container{containerGroup.containers.length > 1 ? 's' : ''}
+                      {displayContainersCount(containerGroup)}
                     </div>
                   </div>
                 </div>
@@ -633,11 +692,17 @@ function errorCallback(container: ContainerInfoUI, errorMessage: string): void {
 
     {#if providerConnections.length === 0}
       <NoContainerEngineEmptyScreen />
-    {:else if $filtered.length === 0}
-      {#if searchTerm}
-        <FilteredEmptyScreen icon="{ContainerIcon}" kind="containers" bind:searchTerm="{searchTerm}" />
+    {:else if containerGroups.length === 0}
+      {#if containerUtils.filterSearchTerm(searchTerm)}
+        <FilteredEmptyScreen
+          icon="{ContainerIcon}"
+          kind="containers"
+          on:resetFilter="{() => (searchTerm = containerUtils.filterResetSearchTerm(searchTerm))}"
+          searchTerm="{containerUtils.filterSearchTerm(searchTerm)}" />
       {:else}
-        <ContainerEmptyScreen />
+        <ContainerEmptyScreen
+          runningOnly="{containerUtils.filterIsRunning(searchTerm)}"
+          stoppedOnly="{containerUtils.filterIsStopped(searchTerm)}" />
       {/if}
     {/if}
   </div>

packages/renderer/src/lib/ui/FilteredEmptyScreen.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 import EmptyScreen from '../ui/EmptyScreen.svelte';
 import Button from '../ui/Button.svelte';
-
+import { createEventDispatcher } from 'svelte';
 export let icon: any;
 export let kind: string;
 export let searchTerm: string;
+
+const dispatch = createEventDispatcher();
+function onResetFilter() {
+  dispatch('resetFilter');
+}
 
 $: filter = searchTerm && searchTerm.length > 20 ? 'filter' : `'${searchTerm}'`;
 </script>
@@ -14,10 +19,5 @@ $: filter = searchTerm && searchTerm.length > 20 ? 'filter' : `'${searchTerm}'`;
   title="No {kind} matching {filter} found"
   message="Not what you expected? Double-check your spelling."
   detail="Just want to view all of your {kind}?">
-  <Button
-    on:click="{() => {
-      searchTerm = '';
-    }}">
-    Clear filter
-  </Button>
+  <Button on:click="{() => onResetFilter()}">Clear filter</Button>
 </EmptyScreen>

packages/renderer/src/lib/container/ContainerEmptyScreen.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
 import EmptyScreen from '../ui/EmptyScreen.svelte';
 import ContainerIcon from '../images/ContainerIcon.svelte';
+export let runningOnly: boolean;
+export let stoppedOnly: boolean;
+
+$: title = getTitle(runningOnly, stoppedOnly);
+$: message = getMessage(stoppedOnly);
+$: commandLine = getCommandLine(stoppedOnly);
+
+function getTitle(runningOnly: boolean, stoppedOnly: boolean): string {
+  if (runningOnly) {
+    return 'No running containers';
+  } else if (stoppedOnly) {
+    return 'No stopped containers';
+  } else {
+    return 'No containers';
+  }
+}
+function getMessage(stoppedOnly: boolean): string {
+  if (stoppedOnly) {
+    return '';
+  } else {
+    return 'Run a first container using the following command line:';
+  }
+}
+function getCommandLine(stoppedOnly: boolean): string {
+  if (stoppedOnly) {
+    return '';
+  } else {
+    return 'podman run quay.io/podman/hello';
+  }
+}
 </script>
 
-<EmptyScreen
-  icon="{ContainerIcon}"
-  title="No containers"
-  message="Run a first container using the following command line:"
-  commandline="podman run quay.io/podman/hello" />
+<EmptyScreen icon="{ContainerIcon}" title="{title}" message="{message}" commandline="{commandLine}" />

packages/renderer/src/lib/container/ContainerInfoUI.ts
@@ -77,6 +77,9 @@ export interface ContainerGroupInfoUI extends ContainerGroupPartInfoUI {
 
   selected: boolean;
 
+  // can be different from containers.length when a filter is applied on containers
+  allContainersCount: number;
+
   // list of containers in this group
   containers: ContainerInfoUI[];
 }

packages/renderer/src/lib/container/ContainerList.spec.ts
@@ -25,6 +25,7 @@ import ContainerList from './ContainerList.svelte';
 import { containersInfos } from '../../stores/containers';
 import { get } from 'svelte/store';
 import { providerInfos } from '../../stores/providers';
+import userEvent from '@testing-library/user-event';
 
 const listContainersMock = vi.fn();
 const getProviderInfosMock = vi.fn();
@@ -74,6 +75,127 @@ test('Expect no container engines being displayed', async () => {
   render(ContainerList);
   const noEngine = screen.getByRole('heading', { name: 'No Container Engine' });
   expect(noEngine).toBeInTheDocument();
+});
+
+test('Expect no containers being displayed', async () => {
+  getProviderInfosMock.mockResolvedValue([
+    {
+      name: 'podman',
+      status: 'started',
+      internalId: 'podman-internal-id',
+      containerConnections: [
+        {
+          name: 'podman-machine-default',
+          status: 'started',
+        },
+      ],
+    },
+  ]);
+
+  window.dispatchEvent(new CustomEvent('extensions-already-started'));
+  window.dispatchEvent(new CustomEvent('provider-lifecycle-change'));
+  window.dispatchEvent(new CustomEvent('tray:update-provider'));
+
+  while (get(providerInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+  await waitRender({});
+
+  const noContainers = screen.getByRole('heading', { name: 'No containers' });
+  expect(noContainers).toBeInTheDocument();
+
+  const runningTab = screen.getByRole('button', { name: 'Running containers' });
+  await fireEvent.click(runningTab);
+
+  const noRunningContainers = screen.getByRole('heading', { name: 'No running containers' });
+  expect(noRunningContainers).toBeInTheDocument();
+
+  const stoppedTab = screen.getByRole('button', { name: 'Stopped containers' });
+  await fireEvent.click(stoppedTab);
+
+  const noStoppedContainers = screen.getByRole('heading', { name: 'No stopped containers' });
+  expect(noStoppedContainers).toBeInTheDocument();
+});
+
+test('Expect is:running / is:stopped is added to the filter field', async () => {
+  getProviderInfosMock.mockResolvedValue([
+    {
+      name: 'podman',
+      status: 'started',
+      internalId: 'podman-internal-id',
+      containerConnections: [
+        {
+          name: 'podman-machine-default',
+          status: 'started',
+        },
+      ],
+    },
+  ]);
+
+  window.dispatchEvent(new CustomEvent('extensions-already-started'));
+  window.dispatchEvent(new CustomEvent('provider-lifecycle-change'));
+  window.dispatchEvent(new CustomEvent('tray:update-provider'));
+
+  while (get(providerInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+  await waitRender({});
+
+  const searchField = screen.getByPlaceholderText('Search containers....');
+  expect(searchField).toBeInTheDocument();
+  expect(searchField).not.toHaveDisplayValue(/is:running/);
+  expect(searchField).not.toHaveDisplayValue(/is:stopped/);
+
+  const runningTab = screen.getByRole('button', { name: 'Running containers' });
+  await fireEvent.click(runningTab);
+
+  expect(searchField).toHaveDisplayValue(/is:running/);
+  expect(searchField).not.toHaveDisplayValue(/is:stopped/);
+
+  const stoppedTab = screen.getByRole('button', { name: 'Stopped containers' });
+  await fireEvent.click(stoppedTab);
+
+  expect(searchField).not.toHaveDisplayValue(/is:running/);
+  expect(searchField).toHaveDisplayValue(/is:stopped/);
+});
+
+test('Expect filter is preserved between tabs', async () => {
+  getProviderInfosMock.mockResolvedValue([
+    {
+      name: 'podman',
+      status: 'started',
+      internalId: 'podman-internal-id',
+      containerConnections: [
+        {
+          name: 'podman-machine-default',
+          status: 'started',
+        },
+      ],
+    },
+  ]);
+
+  window.dispatchEvent(new CustomEvent('extensions-already-started'));
+  window.dispatchEvent(new CustomEvent('provider-lifecycle-change'));
+  window.dispatchEvent(new CustomEvent('tray:update-provider'));
+
+  while (get(providerInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+  await waitRender({});
+
+  const searchField = screen.getByPlaceholderText('Search containers....');
+  expect(searchField).toBeInTheDocument();
+  const user = userEvent.setup();
+  await user.type(searchField, 'foobar');
+  expect(searchField).toHaveDisplayValue(/foobar/);
+
+  const runningTab = screen.getByRole('button', { name: 'Running containers' });
+  await fireEvent.click(runningTab);
+  expect(searchField).toHaveDisplayValue(/foobar/);
+
+  const stoppedTab = screen.getByRole('button', { name: 'Stopped containers' });
+  await fireEvent.click(stoppedTab);
+  expect(searchField).toHaveDisplayValue(/foobar/);
 });
 
 test('Try to delete a pod that has containers', async () => {
@@ -380,4 +502,249 @@ test('Expect filter empty screen', async () => {
 
   const filterButton = screen.getByRole('button', { name: 'Clear filter' });
   expect(filterButton).toBeInTheDocument();
+});
+
+test('Expect clear filter in empty screen to clear serach term, except is:...', async () => {
+  getProviderInfosMock.mockResolvedValue([
+    {
+      name: 'podman',
+      status: 'started',
+      internalId: 'podman-internal-id',
+      containerConnections: [
+        {
+          name: 'podman-machine-default',
+          status: 'started',
+        },
+      ],
+    },
+  ]);
+
+  const singleContainer = {
+    Id: 'sha256:1234567890123',
+    Image: 'sha256:123',
+    Names: ['foo'],
+    Status: 'Running',
+    engineId: 'podman',
+    engineName: 'podman',
+  };
+
+  // one single container
+  const mockedContainers = [singleContainer];
+
+  listContainersMock.mockResolvedValue(mockedContainers);
+
+  window.dispatchEvent(new CustomEvent('extensions-already-started'));
+  window.dispatchEvent(new CustomEvent('provider-lifecycle-change'));
+  window.dispatchEvent(new CustomEvent('tray:update-provider'));
+
+  // wait store are populated
+  while (get(containersInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+
+  while (get(providerInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+  await waitRender({});
+
+  const searchField = screen.getByPlaceholderText('Search containers....');
+  expect(searchField).toBeInTheDocument();
+  const user = userEvent.setup();
+  await user.type(searchField, 'foobar');
+  expect(searchField).toHaveDisplayValue(/foobar/);
+
+  const runningTab = screen.getByRole('button', { name: 'Running containers' });
+  await fireEvent.click(runningTab);
+  expect(searchField).toHaveDisplayValue(/foobar/);
+  expect(searchField).toHaveDisplayValue(/is:running/);
+
+  const filterButton = screen.getByRole('button', { name: 'Clear filter' });
+  expect(filterButton).toBeInTheDocument();
+
+  await fireEvent.click(filterButton);
+  expect(searchField).not.toHaveDisplayValue(/foobar/);
+  expect(searchField).toHaveDisplayValue(/is:running/);
+});
+
+test('Expect to display running / stopped containers depending on tab', async () => {
+  getProviderInfosMock.mockResolvedValue([
+    {
+      name: 'podman',
+      status: 'started',
+      internalId: 'podman-internal-id',
+      containerConnections: [
+        {
+          name: 'podman-machine-default',
+          status: 'started',
+        },
+      ],
+    },
+  ]);
+
+  const pod1Id = 'pod1-id';
+  const pod2Id = 'pod2-id';
+  const pod3Id = 'pod3-id';
+
+  const firstId = 'sha256:68347658374683476';
+
+  // one single container and two containers part of a pod
+  const mockedContainers = [
+    // 2 / 2 containers are running on this pod
+    {
+      Id: firstId,
+      Image: 'sha256:234',
+      Names: ['container1-pod1'],
+      RepoTags: ['veryold:image'],
+      State: 'Running',
+      pod: {
+        name: 'pod1',
+        id: pod1Id,
+        status: 'Running',
+      },
+      engineId: 'podman',
+      engineName: 'podman',
+    },
+    {
+      Id: 'sha256:7897891234567890123',
+      Image: 'sha256:345',
+      Names: ['container2-pod1'],
+      State: 'Running',
+      pod: {
+        name: 'pod1',
+        id: pod1Id,
+        status: 'Running',
+      },
+      engineId: 'podman',
+      engineName: 'podman',
+    },
+
+    // 1 / 2 containers are running on this pod
+    {
+      Id: 'sha256:876532948235',
+      Image: 'sha256:876',
+      Names: ['container1-pod2'],
+      RepoTags: ['veryold:image'],
+      State: 'Running',
+      pod: {
+        name: 'pod2',
+        id: pod2Id,
+        status: 'Running',
+      },
+      engineId: 'podman',
+      engineName: 'podman',
+    },
+    {
+      Id: 'sha256:834752375490',
+      Image: 'sha256:834',
+      Names: ['container2-pod2'],
+      State: 'Stopped',
+      pod: {
+        name: 'pod2',
+        id: pod2Id,
+        status: 'Running',
+      },
+      engineId: 'podman',
+      engineName: 'podman',
+    },
+
+    // 0 / 2 containers are running on this pod
+    {
+      Id: 'sha256:56283769268',
+      Image: 'sha256:562',
+      Names: ['container1-pod3'],
+      RepoTags: ['veryold:image'],
+      State: 'Stopped',
+      pod: {
+        name: 'pod3',
+        id: pod3Id,
+        status: 'Stopped',
+      },
+      engineId: 'podman',
+      engineName: 'podman',
+    },
+    {
+      Id: 'sha256:834752375490',
+      Image: 'sha256:834',
+      Names: ['container2-pod3'],
+      State: 'Stopped',
+      pod: {
+        name: 'pod3',
+        id: pod3Id,
+        status: 'Stopped',
+      },
+      engineId: 'podman',
+      engineName: 'podman',
+    },
+  ];
+
+  listContainersMock.mockResolvedValue(mockedContainers);
+
+  window.dispatchEvent(new CustomEvent('extensions-already-started'));
+  window.dispatchEvent(new CustomEvent('provider-lifecycle-change'));
+  window.dispatchEvent(new CustomEvent('tray:update-provider'));
+
+  // wait store are populated
+  while (get(containersInfos).length === 0 || get(containersInfos)[0].Id !== firstId) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+
+  while (get(providerInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+  await waitRender({});
+
+  const tests = [
+    {
+      tabLabel: undefined,
+      presentCells: [
+        'pod1 (pod) 2 containers',
+        'container1-pod1 RUNNING',
+        'container2-pod1 RUNNING',
+        'pod2 (pod) 2 containers',
+        'container1-pod2 RUNNING',
+        'container2-pod2 STOPPED',
+        'pod3 (pod) 2 containers',
+        'container1-pod3 STOPPED',
+        'container2-pod3 STOPPED',
+      ],
+      absentLabels: [],
+    },
+    {
+      tabLabel: 'Running containers',
+      presentCells: [
+        'pod1 (pod) 2 containers',
+        'container1-pod1 RUNNING',
+        'container2-pod1 RUNNING',
+        'pod2 (pod) 2 containers (1 filtered)',
+        'container1-pod2 RUNNING',
+      ],
+      absentLabels: [/container2-pod2.*/, /pod3 \(pod\).*/, /container1-pod3.*/, /container2-pod3.*/],
+    },
+    {
+      tabLabel: 'Stopped containers',
+      presentCells: [
+        'pod2 (pod) 2 containers (1 filtered)',
+        'container2-pod2 STOPPED',
+        'pod3 (pod) 2 containers',
+        'container1-pod3 STOPPED',
+        'container2-pod3 STOPPED',
+      ],
+      absentLabels: [/pod1 \(pod\).*/, /container1-pod1.*/, /container2-pod1.*/, /container1-pod2.*/],
+    },
+  ];
+
+  for (const tt of tests) {
+    if (tt.tabLabel) {
+      const tab = screen.getByRole('button', { name: tt.tabLabel });
+      await fireEvent.click(tab);
+    }
+    for (const presentCell of tt.presentCells) {
+      const cell = screen.getByRole('cell', { name: presentCell });
+      expect(cell).toBeInTheDocument();
+    }
+    for (const absentCell of tt.absentLabels) {
+      const cell = screen.queryByRole('cell', { name: absentCell });
+      expect(cell).not.toBeInTheDocument();
+    }
+  }
 });

packages/renderer/src/lib/container/container-utils.ts
@@ -193,7 +193,13 @@ export class ContainerUtils {
       const group = containerInfo.groupInfo;
       if (group.type === ContainerGroupInfoTypeUI.STANDALONE) {
         // standalone group, insert with id as key
-        groups.set(containerInfo.id, { ...group, containers: [containerInfo], expanded: true, selected: false });
+        groups.set(containerInfo.id, {
+          ...group,
+          containers: [containerInfo],
+          expanded: true,
+          selected: false,
+          allContainersCount: 1,
+        });
       } else {
         if (!groups.has(group.name)) {
           groups.set(group.name, {
@@ -205,12 +211,14 @@ export class ContainerUtils {
             status: group.status,
             engineId: group.engineId,
             engineType: group.engineType,
+            allContainersCount: 0,
             containers: [],
           });
         }
         groups.get(group.name)?.containers.push(containerInfo);
       }
     });
+    groups.forEach(group => (group.allContainersCount = group.containers.length));
 
     Array.from(groups.values())
       .filter(group => group.type === ContainerGroupInfoTypeUI.COMPOSE)
@@ -267,5 +275,50 @@ export class ContainerUtils {
 
   adaptContextOnContainer(context: ContextUI, container: ContainerInfo): void {
     context.setValue('containerLabelKeys', container.Labels ? Object.keys(container.Labels) : []);
+  }
+
+  filterResetRunning(f: string) {
+    return f
+      .split(' ')
+      .filter(part => !part.startsWith('is:running') && !part.startsWith('is:stopped'))
+      .join(' ');
+  }
+
+  filterSetRunning(f: string) {
+    const parts = f.split(' ').filter(part => !part.startsWith('is:running') && !part.startsWith('is:stopped'));
+    parts.push('is:running');
+    return parts.join(' ');
+  }
+
+  filterSetStopped(f: string) {
+    const parts = f.split(' ').filter(part => !part.startsWith('is:running') && !part.startsWith('is:stopped'));
+    parts.push('is:stopped');
+    return parts.join(' ');
+  }
+
+  filterResetSearchTerm(f: string): string {
+    return f
+      .split(' ')
+      .filter(part => part.startsWith('is:'))
+      .join(' ');
+  }
+
+  filterIsRunning(f: string): boolean {
+    return f.split(' ').includes('is:running');
+  }
+
+  filterIsStopped(f: string): boolean {
+    return f.split(' ').includes('is:stopped');
+  }
+
+  filterIsAll(f: string): boolean {
+    return !this.filterIsRunning(f) && !this.filterIsStopped(f);
+  }
+
+  filterSearchTerm(f: string): string {
+    return f
+      .split(' ')
+      .filter(part => !part.startsWith('is:'))
+      .join(' ');
   }
 }

commit baa790b5ad182a36b03ad6c56d02810f24476b18

fix: empty screen reset filter by default
Fixes the regression caused by #4600. Prior to this the FilteredEmptyScreen
would reset all filters to ''. The container list needed special processing
to not remove the filters like 'is:running' so it was changed to an event,
but none of the other lists respond to this event so none of the filters get
cleared.

This sets a default behaviour of clearing the list so that none of the other
lists need to respond to the event if they are fine just clearing the filter.
If you do your own special processing, you just call e.preventDefault(). The
search term is included as an event property because we need to set
cancelable in the third argument.

The pods list has the tabs/is:running so I added the event handling there as
well. Without this change clearing the filter would always return you to the
first tab.

Fixes #5288.

Signed-off-by: Tim deBoer <git@tdeboer.ca>

packages/renderer/src/lib/container/ContainerList.svelte
@@ -702,7 +702,10 @@ function setStoppedFilter() {
         <FilteredEmptyScreen
           icon="{ContainerIcon}"
           kind="containers"
-          on:resetFilter="{() => (searchTerm = containerUtils.filterResetSearchTerm(searchTerm))}"
+          on:resetFilter="{e => {
+            searchTerm = containerUtils.filterResetSearchTerm(searchTerm);
+            e.preventDefault();
+          }}"
           searchTerm="{containerUtils.filterSearchTerm(searchTerm)}" />
       {:else}
         <ContainerEmptyScreen

packages/renderer/src/lib/pod/PodsList.svelte
@@ -335,7 +335,14 @@ function computeInterval(): number {
       <NoContainerEngineEmptyScreen />
     {:else if $filtered.length === 0}
       {#if searchTerm}
-        <FilteredEmptyScreen icon="{PodIcon}" kind="pods" bind:searchTerm="{searchTerm}" />
+        <FilteredEmptyScreen
+          icon="{PodIcon}"
+          kind="pods"
+          bind:searchTerm="{searchTerm}"
+          on:resetFilter="{e => {
+            searchTerm = podUtils.filterResetSearchTerm(searchTerm);
+            e.preventDefault();
+          }}" />
       {:else}
         <PodEmptyScreen />
       {/if}

packages/renderer/src/lib/pod/pod-utils.ts
@@ -86,6 +86,13 @@ export class PodUtils {
       return uniqueName;
     }
   }
+
+  filterResetSearchTerm(f: string): string {
+    return f
+      .split(' ')
+      .filter(part => part.startsWith('is:'))
+      .join(' ');
+  }
 }
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any

packages/renderer/src/lib/ui/FilteredEmptyScreen.svelte
@@ -8,7 +8,9 @@ export let searchTerm: string;
 
 const dispatch = createEventDispatcher();
 function onResetFilter() {
-  dispatch('resetFilter');
+  if (dispatch('resetFilter', searchTerm, { cancelable: true })) {
+    searchTerm = '';
+  }
 }
 
 $: filter = searchTerm && searchTerm.length > 20 ? 'filter' : `'${searchTerm}'`;